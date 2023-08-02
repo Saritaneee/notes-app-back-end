1825dc5c060baa082a7f9ea5908ca37647ba5b9b
Tane <intan.mp21@gmail.com>
2023-08-02 20:30:57 +0700

    fix bugs

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {     //cross-origin-sharing-policy (response.header('Access-Control-Allow-Origin', '(client-web');)
         origin: ['*'],
